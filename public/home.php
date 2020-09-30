9613547716ea0816f27d40c83316227400dcbd6a
Delphine <delphine.pva@gmail.com>
2020-09-30 13:22:47 +0200

    mise à jour de home.php

---
+++ public/home.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../src/style.css" />
+    <title>Document</title>
+</head>
+<body>
+  
+    <header>
+        <video autoplay loop muted class="video-background">
+            <source src="../src/img/Video.mp4" type="video/mp4">
+        </video>
+    </header>
+     
+    <section>
+        <h2>Le jeu</h2>
+
+        <p>Fishing Sim World Pro Tour est un simulateur hautement réaliste qui vous fera voyager et vivre des expériences de pêche sans précédent. 
+        Ressentez l’adrénaline monter lorsque vous attraperez de monstrueux bars, de grands brochets ou de gigantesques carpes ! 
+        Fishing Sim World dispose de physiques en jeu réalistes et de poissons IA authentiques qui rendront cette expérience aussi immersible que possible.</p>
+
+        <p>Vos talents de pêcheur seront mis à l’épreuve tandis que vous explorerez les lacs des USA, situés en Floride et à New-York, 
+        ainsi que 5 lacs européens, tels que celui de Gigantica en France et le Grand Union Canal au Royaume-Uni.</p>
+
+        <p>Traquez les 18 espèces de poissons différentes et collectionnez des centaines d’équipements, y compris des objets de grandes marques 
+        sous licence comme Bass Cat Boats, Rat-L-Trap, Duckett Fishing et Korda. Utilisez le GPS de votre bateau et le sonar pour 
+        localiser les bars avant de choisir la bonne technique qui vous permettra de les capturer.</p>
+
+        <p>Grâce à des systèmes IA avancés, les poissons et les types d'espèces ont tous des comportements différents. Ils ne réagiront pas de l
+        a même manière aux températures de l'eau et aux appâts proposés, vous devrez donc faire preuve de stratégie pour attraper vos proies. 
+        Pêchez à votre rythme et trouvez l’endroit parfait, puis mettez-vous au défi d’attraper le plus beau poisson.</p>
+        </p>
+    </section>
+
+    <section>
+        <h2>Dernières news</h2>
+
+    <!-- Ultimate Fishing Simulator disponible sur Xbox One
+    https://www.simulagri.fr/2020/05/ultimate-fishing-simulator-disponible-sur-xbox-one/ -->
+    
+    <!--La pêche en réalité virtuelle, c’est juste dingue !
+    https://www.simulagri.fr/2019/09/ultimate-fishing-simulator-vr-la-peche-en-realite-virtuelle-cest-juste-dingue/ -->
+
+
+        <div class="bloc-container">
+            
+            <div class="bloc">Voir l'article</div>
+            <div class="bloc">Voir l'article</div>
+            <div class="bloc">voir l'article</div>
+            <div class="bloc">Voir l'article</div>
+        </div>
+
+        <a href="#" class="btn btn-article">Voir l'article</a>
+        <a href="#" class="btn btn-article">Voir l'article</a>
+        <a href="#" class="btn btn-article">Voir l'article</a>
+        <a href="#" class="btn btn-article">Voir l'article</a>
+    
+
+    </section>
+  
+</body>
+</html>
