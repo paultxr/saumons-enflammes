bff6103ebf3d8fe8636b3d077880936d6ab6e510
Guillaume <gdv.delvecchio75@gmail.com>
2020-09-30 14:48:30 +0200

    add css Version 1 on footer

---
+++ public/contact.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contactez nous </h1>
+    <form action="" method="post">
+        <div>
+            <label for="firstName">Prénom :</label>
+            <input type="text" id="firstName" name="userFirstName">
+        </div>
+        <div>
+            <label for="lastName">Nom :</label>
+            <input type="text" id="lastName" name="userLastName">
+        </div>
+        <div>
+            <label for="msg">Votre message :</label>
+            <textarea id="msg" name="userMessage"></textarea>
+        </div>
+        <div class="button">
+            <button type="submit">Envoyer</button>
+        </div>
+
+    </form>
+</body>
+</html>
