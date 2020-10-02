c56c50588bccb7ec60442b8d5bb341c2c182b81f
Paul Texier <txrpaul@gmail.com>
2020-10-02 10:10:30 +0200

    added cover

--- public/shop.php
+++ public/shop.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="../src/style.css" rel="stylesheet">
+    <title>L'équipe des saumons</title>
 </head>
 <body>
-    
+
+    <div class="parallax">
+
+        <div class=title-background><h1>L'équipe des saumons</h1></div>
+
+    </div>
 </body>
 </html>
