8bde5704a51919e99b8a9f34df3c6502811fc1ed
Paul Texier <txrpaul@gmail.com>
2020-10-02 12:22:05 +0200

    new articles

--- public/shop.php
+++ public/shop.php
@@ -8,26 +8,69 @@
 </head>
 <body>
 
-    <div class="parallax">
+    <div class="parallax-boutique">
 
         <div class=title-background><h1>La poissonnerie</h1></div>
 
     </div>
 
-    <section >
+    <section class="shopping-content">
 
-        <div class="filter">FILTER</div> 
+             <article>
 
-        <div class="shopping">
-            <article><img src=""></article>
-            <article><img src=""></article>
-            <article><img src=""></article>
-            <article><img src=""></article>
-            <article><img src=""></article>
-            <article><img src=""></article>
-            <article><img src=""></article>
-            <article><img src=""></article>
-            <article><img src=""></article>
+                <img src="../src/img/peche-simulateur.jpg"  class="shopping-img" width=80% alt="fishing-simulator">
+                <h3>SET UP : Simulateur de pêche pour salon</h3>
+                <p class="price">2349.99 €</p>
+                
+            </article>
+
+
+            <article>
+                <img src="../src/img/hat.jpg" class="shopping-img" alt="hat" width="80%">
+                <h3>Parapluie de tête motif camouflage</h3>
+                <p class="price">8.99 €</p>
+            </article>
+
+            <article>
+                 
+                <img src="../src/img/fish-shoes3.jpg"  class="shopping-img" width=80% alt="fishing-shoes">
+                <h3>Pantoufles truite</h3>
+                <p class="price">14.99 €</p>
+                
+            </article>
+
+            <article>
+                <img src="../src/img/singing-salmon.jpg" alt="singing salmon" class="shopping-img" width="80%">
+                <h3>Saumon chantant Fixation Facile</h3>
+                <p class="price">12.99 €</p>
+            </article>
+
+            <article>
+                <img src="../src/img/canne-peche.jpg" alt="fishing-cane" class="shopping-img" width="80%">
+                <h3>Canne à pêche bluetooth</h3>
+                <p class="price">34.99 €</p>
+            </article>
+
+
+            <article>
+                <img src="../src/img/veste.jpg" class="shopping-img" width=80% alt="jacket">
+                <h3>Véritable veste de pêcheur</h3>
+                <p class="price">8.99 €</p>
+                
+            </article>
+
+            <article>
+                <img src="../src/img/cuvette-océan.jpg" class="shopping-img" alt="fish-toilets" width="80%">
+                <h3>Cuvette Océan</h3>
+                <p class="price">13.90 €</p>
+            </article>
+
+            <article>
+                <img src="../src/img/veste.jpg" class="shopping-img" width=80% alt="jacket">
+                <h3>Véritable veste de pêcheur</h3>
+                <p class="price">8.99 €</p>
+            </article>
+          
         </div>
 
     </section>
