ccabea4694154f76e24d079d7256bb86114cbc1f
Paul Texier <txrpaul@gmail.com>
2020-10-04 14:19:48 +0200

    made it responsive

--- public/shop.php
+++ public/shop.php
@@ -28,7 +28,7 @@
                 <img src="../src/img/peche-simulateur.jpg"  class="shopping-img" width=80% alt="fishing-simulator">
                 <h3>SET UP : Simulateur de pêche pour salon</h3>
                 <p class="price">2349.99 €</p>
-                <input class="favorite styled" type="button" value="Acheter">
+                <input class="submit-button" type="button" value="Acheter">
                 
             </article>
 
@@ -37,7 +37,7 @@
                 <img src="../src/img/hat.jpg" class="shopping-img" alt="hat" width="80%">
                 <h3>Parapluie de tête motif camouflage</h3>
                 <p class="price">8.99 €</p>
-                <input class="favorite styled" type="button" value="Acheter">
+                <input class="submit-button" type="button" value="Acheter">
             </article>
 
             <article>
@@ -45,7 +45,7 @@
                 <img src="../src/img/fish-shoes.jpg"  class="shopping-img" width=80% alt="fishing-shoes">
                 <h3>Pantoufles truite</h3>
                 <p class="price">14.99 €</p>
-                <input class="favorite styled" type="button" value="Acheter">
+                <input class="submit-button" type="button" value="Acheter">
                 
             </article>
 
@@ -53,14 +53,14 @@
                 <img src="../src/img/singing-salmon.jpg" alt="singing salmon" class="shopping-img" width="80%">
                 <h3>Saumon chantant Fixation Facile</h3>
                 <p class="price">12.99 €</p>
-                <input class="favorite styled" type="button" value="Acheter">
+                <input class="submit-button" type="button" value="Acheter">
             </article>
 
             <article>
                 <img src="../src/img/canne-peche.jpg" alt="fishing-cane" class="shopping-img" width="80%">
                 <h3>Canne à pêche bluetooth</h3>
                 <p class="price">34.99 €</p>
-                <input class="favorite styled" type="button" value="Acheter">
+                <input class="submit-button" type="button" value="Acheter">
             </article>
 
 
@@ -68,7 +68,7 @@
                 <img src="../src/img/veste.jpg" class="shopping-img" width=80% alt="jacket">
                 <h3>Véritable veste de pêcheur</h3>
                 <p class="price">8.99 €</p>
-                <input class="favorite styled" type="button" value="Acheter">
+                <input class="submit-button" type="button" value="Acheter">
                 
             </article>
 
@@ -76,14 +76,14 @@
                 <img src="../src/img/cuvette.jpg" class="shopping-img" alt="fish-toilets" width="80%">
                 <h3>Cuvette qui donne la pêche</h3>
                 <p class="price">13.90 €</p>
-                <input class="favorite styled" type="button" value="Acheter">
+                <input class="submit-button" type="button" value="Acheter">
             </article>
 
             <article>
                 <img src="../src/img/poisson-pour-chat.jpg" class="shopping-img" width=80% alt="poisson-pour-chat">
                 <h3>Poisson en plastique pour chat</h3>
                 <p class="price">8.99 €</p>
-                <input class="favorite styled" type="button" value="Acheter">
+                <input class="submit-button"type="button" value="Acheter">
             </article>
           
         </div>
