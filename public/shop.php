b0e165c3423a3ee2f70019862668864e91e29573
Paul Texier <txrpaul@gmail.com>
2020-10-04 13:49:38 +0200

    added button

--- public/shop.php
+++ public/shop.php
@@ -14,6 +14,13 @@
 
     </div>
 
+    <section class="intro">
+
+        <h2>GOODIES ET ACCESSOIRES</h2>
+        <p>Optez pour les nouveaux styles de la saison avec de grosses baskets et des chaussures en cuir élégantes d’ASOS DESIGN, ou essayez une paire d’Adidas Originals à lacets en daim brossé. Si vous devez renouveler vos paires de chaussures pour l’été, alliez confort et style en alternant entre chaussures bateau et tongs de marque. Faites que votre vie soit aussi colorée et inattendue que cette paire de bottines Chelsea à la couleur et aux motifs texturés extraordinaires. Vous cherchez un basique ? Nous pouvons vous aider à trouver ce qu’il vous faut grâce à notre gamme de Desert boots disponibles dans une palette de couleurs neutres qui vont avec tout, elles seront parfaites pour tous les jours.</p>
+
+    </section>
+
     <section class="shopping-content">
 
              <article>
@@ -21,6 +28,7 @@
                 <img src="../src/img/peche-simulateur.jpg"  class="shopping-img" width=80% alt="fishing-simulator">
                 <h3>SET UP : Simulateur de pêche pour salon</h3>
                 <p class="price">2349.99 €</p>
+                <input class="favorite styled" type="button" value="Acheter">
                 
             </article>
 
@@ -29,13 +37,15 @@
                 <img src="../src/img/hat.jpg" class="shopping-img" alt="hat" width="80%">
                 <h3>Parapluie de tête motif camouflage</h3>
                 <p class="price">8.99 €</p>
+                <input class="favorite styled" type="button" value="Acheter">
             </article>
 
             <article>
                  
-                <img src="../src/img/fish-shoes3.jpg"  class="shopping-img" width=80% alt="fishing-shoes">
+                <img src="../src/img/fish-shoes.jpg"  class="shopping-img" width=80% alt="fishing-shoes">
                 <h3>Pantoufles truite</h3>
                 <p class="price">14.99 €</p>
+                <input class="favorite styled" type="button" value="Acheter">
                 
             </article>
 
@@ -43,12 +53,14 @@
                 <img src="../src/img/singing-salmon.jpg" alt="singing salmon" class="shopping-img" width="80%">
                 <h3>Saumon chantant Fixation Facile</h3>
                 <p class="price">12.99 €</p>
+                <input class="favorite styled" type="button" value="Acheter">
             </article>
 
             <article>
                 <img src="../src/img/canne-peche.jpg" alt="fishing-cane" class="shopping-img" width="80%">
                 <h3>Canne à pêche bluetooth</h3>
                 <p class="price">34.99 €</p>
+                <input class="favorite styled" type="button" value="Acheter">
             </article>
 
 
@@ -56,6 +68,7 @@
                 <img src="../src/img/veste.jpg" class="shopping-img" width=80% alt="jacket">
                 <h3>Véritable veste de pêcheur</h3>
                 <p class="price">8.99 €</p>
+                <input class="favorite styled" type="button" value="Acheter">
                 
             </article>
 
@@ -63,12 +76,14 @@
                 <img src="../src/img/cuvette.jpg" class="shopping-img" alt="fish-toilets" width="80%">
                 <h3>Cuvette qui donne la pêche</h3>
                 <p class="price">13.90 €</p>
+                <input class="favorite styled" type="button" value="Acheter">
             </article>
 
             <article>
                 <img src="../src/img/poisson-pour-chat.jpg" class="shopping-img" width=80% alt="poisson-pour-chat">
                 <h3>Poisson en plastique pour chat</h3>
                 <p class="price">8.99 €</p>
+                <input class="favorite styled" type="button" value="Acheter">
             </article>
           
         </div>
