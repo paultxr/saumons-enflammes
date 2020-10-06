24611df7e745c7f20161f421ca5a6767318fe407
Ali <ma.ramzan089@gmail.com>
2020-10-06 15:41:00 +0200

    CSS update

--- public/header.php
+++ public/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/src/style.css">
+    <link rel="stylesheet" href="../src/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Nav bar responsive</title>
 </head>
@@ -24,6 +24,6 @@
             </ul>
         </nav>
     </header>
-    <script src="/src/script.js"></script>
+    <script src="../src/script.js"></script>
 </body>
 </html>
