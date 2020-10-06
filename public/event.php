26149bb6d3300744acc45caa5bccb484c90dbb8b
maximilienpy <max314116@gmail.com>
2020-10-06 15:46:15 +0200

    Modification du event.php avec un article, plus photo fon d'écran Sur le CSS il y a l'ajout du parallax et copier/coller du script de Delphine pour reprendre l'architecture de la page  la modification de Delphine de son fichier script.js sur la branche event.

---
+++ public/event.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../src/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet"> 
+    <title>Les compèts  -- events</title>
+
+
+</head>
+
+<body>
+
+    
+<section id= 'banner'>
+
+    <div class="parallax" alt="the team">
+        <div class=title-background><h1>Evènements et compétitions</h1></div>
+
+    </div>
+
+</section>
+
+    
+   <section>
+        <div class="cardEvent">
+            <div class="cardArticle">
+                <div class="cardHeaderbis">
+                    <div class="cardPic"></div>
+                    <div class="cardBodybis">
+                        <h4 class="cardTitlebis">Montréal 2021</h4>
+                        <p class="cardTexte">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+                        <a class="cardLinkbis" href="https://www.mlg.com/schedule"><button class="cardButtonbis">Voir article</button></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<script src="../src/script.js"></script>
+
+</body>
+</html>
---
+++ src/script.js
@@ -0,0 +1,66 @@
+const cards = document.querySelector(".cardEvent");
+
+const articles = [
+  {
+    mytitle: "Ultimate Fishing Simulator enchaîne les mises à jour",
+    picture: "https://cdn.pixabay.com/photo/2019/07/22/10/42/boat-4354777_1280.jpg",
+    mytext: "lorem20",
+    myLink: "https://www.simulagri.fr/2019/11/ultimate-fishing-simulator-enchaine-les-mises-a-jour-dlc-amazon-river-et-vr/"
+  },
+  {
+    mytitle: "La pêche en réalité virtuelle, c’est juste dingue !",
+    picture: "https://www.heypoorplayer.com/wp-content/uploads/2016/11/DBA67C87-804C-4FC9-BB9D-CEC46831116C-18204-000008F60A28B53F_tmp.jpg",
+    mytext: "lorem20",
+    myLink: "https://www.simulagri.fr/2019/09/ultimate-fishing-simulator-vr-la-peche-en-realite-virtuelle-cest-juste-dingue/"
+},
+  {
+    mytitle: "La pêche à la ligne bientôt en réalité virtuelle !",
+    picture: "https://cdn.pixabay.com/photo/2015/06/04/23/06/fisherman-797980_1280.jpg",
+    mytext: "lorem20", 
+    myLink: "https://www.simulagri.fr/2019/08/la-peche-a-la-ligne-bientot-en-vr-sur-ultimate-fishing-simulator/"
+} 
+];
+
+function createCard(title, imageUrl, linkUrl, texte) {
+  const cardArticle = document.createElement("div");
+  cardArticle.classList.add("cardArticle");
+  cardEvent.appendChild(cardArticle);
+
+  const cardHeaderbis = document.createElement("div");
+  cardHeaderbis.classList.add("cardHeaderbis");
+  cardArticle.appendChild(cardHeaderbis);
+
+  const cardPic = document.createElement("div");
+  cardPic.style.backgroundImage = `url(${imageUrl})`;
+  cardPic.classList.add("cardPic");
+  cardHeaderbis.appendChild(cardPic);
+
+  const cardBodybis = document.createElement("div");
+  cardBodybis.classList.add("cardBodybis");
+  cardArticle.appendChild(cardBodybis);
+
+  const cardTitlebis = document.createElement("h4");
+  cardTitlebis.classList.add("cardTitlebis");
+  cardTitlebis.innerHTML = `${title}`;
+  cardBodybis.appendChild(cardTitlebis);
+  
+  const cardTexte = document.createElement("p");
+  cardTexte.classList.add("cardTexte");
+  cardTexte.innerHTML = `${texte}`;
+  cardBodybis.appendChild(cardTexte);
+
+  const cardLinkbis = document.createElement("a");
+  cardLinkbis.classList.add("cardLinkbis");
+  cardLinkbis.href = `${linkUrl}`;
+  cardBodybis.appendChild(cardLinkbis);
+
+  const cardButtonbis = document.createElement("button");
+  cardButtonbis.classList.add("cardButtonbis");
+  cardButtonbis.innerHTML = "Voir l'article";
+  cardLinkbis.appendChild(cardButtonbis);
+
+}
+
+for (let i = 0; i < articles.length; i++) {
+  createCard(articles[i].mytitle, articles[i].picture, articles[i].mytext, articles[i].myLink);
+}
