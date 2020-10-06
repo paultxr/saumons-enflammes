b98e8a27c9801933fe7c5d453f6d61d81d09068d
Paul Texier <txrpaul@gmail.com>
2020-10-06 09:15:53 +0200

    last modifs

--- public/shop.php
+++ public/shop.php
@@ -17,77 +17,99 @@
     <section class="intro">
 
         <h2>GOODIES ET ACCESSOIRES</h2>
-        <p>Optez pour les nouveaux styles de la saison avec de grosses baskets et des chaussures en cuir élégantes d’ASOS DESIGN, ou essayez une paire d’Adidas Originals à lacets en daim brossé. Si vous devez renouveler vos paires de chaussures pour l’été, alliez confort et style en alternant entre chaussures bateau et tongs de marque. Faites que votre vie soit aussi colorée et inattendue que cette paire de bottines Chelsea à la couleur et aux motifs texturés extraordinaires. Vous cherchez un basique ? Nous pouvons vous aider à trouver ce qu’il vous faut grâce à notre gamme de Desert boots disponibles dans une palette de couleurs neutres qui vont avec tout, elles seront parfaites pour tous les jours.</p>
+        <p>Retrouvez ici tous les accessoires indispensables pour devenir un expert sur Fishing Sim World Tour ! Vivez une vraie séance de pêche depuis votre salon grâce au "Fishing Simulateur" ! Marre des fils de pêche ? Aucun problème, il y a la canne à pêche bluetooth ! Exprimez votre passion pour la pêche en portant des superbes sandales en forme de poisson ou en décorant votre mur avec le saumon chantant ! Il y a forcément quelque chose qui vous correspond dans "la poissonerie".</p>
 
     </section>
 
+    
+
+ <!--   <div class="dropdown">
+    <div id="cart">
+        <p><span id="in-cart-items-num">0</span> Articles</p>
+    </div> 
+
+    <ul id="cart-dropdown" hidden>
+        <li id="empty-cart-msg"><a>Votre panier est vide</a></li>
+        <li class="go-to-cart hidden"><a href="/panier/">Voir le panier</a></li>
+    </ul>
+</div> -->
+
     <section class="shopping-content">
 
-             <article id="fishing-simulator">
-
+             <form id="fishing-simulator">
                 <img src="../src/img/peche-simulateur.jpg"  class="shopping-img" width=80% alt="fishing-simulator">
                 <h3>SET UP : Simulateur de pêche pour salon</h3>
                 <p class="price">2349.99 €</p>
-                <input class="submit-button" type="button" value="Acheter">
+                input
+               <input >
+
                 
-            </article>
+            </form>
 
 
-            <article id="fishing-simulator">
-                <img src="../src/img/hat.jpg" class="shopping-img" alt="hat" width="80%">
+            <form id="hat">
+                <img src="../src/img/hat.jpg" class="shopping-img" alt="bob de pêche" width="80%">
                 <h3>Parapluie de tête motif camouflage</h3>
                 <p class="price">8.99 €</p>
-                <input class="submit-button" type="button" value="Acheter">
-            </article>
+                <a href="panier.php?action=ajout&amp;l=LIBELLEPRODUIT&amp;q=QUANTITEPRODUIT&amp;p=PRIXPRODUIT" onclick="window.open(this.href, '', 
+                'toolbar=no, location=no, directories=no, status=yes, scrollbars=yes, resizable=yes, copyhistory=no, width=600, height=350'); return false;">Ajouter au panier</a>
+            </form>
 
-            <article id="shoes">
+            <form id="shoes">
                  
                 <img src="../src/img/fish-shoes.jpg"  class="shopping-img" width=80% alt="fishing-shoes">
                 <h3>Pantoufles truite</h3>
                 <p class="price">14.99 €</p>
-                <input class="submit-button" type="button" value="Acheter">
+                <a href="panier.php?action=ajout&amp;l=LIBELLEPRODUIT&amp;q=QUANTITEPRODUIT&amp;p=PRIXPRODUIT" onclick="window.open(this.href, '', 
+                'toolbar=no, location=no, directories=no, status=yes, scrollbars=yes, resizable=yes, copyhistory=no, width=600, height=350'); return false;">Ajouter au panier</a>
                 
-            </article>
+            </form>
 
-            <article id="fishing-simulator">
+            <form id="singing-salmon">
                 <img src="../src/img/singing-salmon.jpg" alt="singing salmon" class="shopping-img" width="80%">
                 <h3>Saumon chantant Fixation Facile</h3>
                 <p class="price">12.99 €</p>
-                <input class="submit-button" type="button" value="Acheter">
-            </article>
+                <a href="panier.php?action=ajout&amp;l=LIBELLEPRODUIT&amp;q=QUANTITEPRODUIT&amp;p=PRIXPRODUIT" onclick="window.open(this.href, '', 
+                'toolbar=no, location=no, directories=no, status=yes, scrollbars=yes, resizable=yes, copyhistory=no, width=600, height=350'); return false;">Ajouter au panier</a>
+            </form>
 
-            <article id="fishing-cane">
+            <form id="fishing-cane">
                 <img src="../src/img/canne-peche.jpg" alt="fishing-cane" class="shopping-img" width="80%">
                 <h3>Canne à pêche bluetooth</h3>
                 <p class="price">34.99 €</p>
-                <input class="submit-button" type="button" value="Acheter">
-            </article>
+                <a href="panier.php?action=ajout&amp;l=LIBELLEPRODUIT&amp;q=QUANTITEPRODUIT&amp;p=PRIXPRODUIT" onclick="window.open(this.href, '', 
+                'toolbar=no, location=no, directories=no, status=yes, scrollbars=yes, resizable=yes, copyhistory=no, width=600, height=350'); return false;">Ajouter au panier</a>
+            </form>
 
 
-            <article id="jacket">
+            <form id="jacket">
                 <img src="../src/img/veste.jpg" class="shopping-img" width=80% alt="jacket">
                 <h3>Véritable veste de pêcheur</h3>
-                <p class="price">8.99 €</p>
-                <input class="submit-button" type="button" value="Acheter">
+                <p class="price">28.99 €</p>
+                <a href="panier.php?action=ajout&amp;l=LIBELLEPRODUIT&amp;q=QUANTITEPRODUIT&amp;p=PRIXPRODUIT" onclick="window.open(this.href, '', 
+                'toolbar=no, location=no, directories=no, status=yes, scrollbars=yes, resizable=yes, copyhistory=no, width=600, height=350'); return false;">Ajouter au panier</a>
                 
-            </article>
+            </form>
 
-            <article id="toilet-bowl">
+            <form id="toilet-bowl">
                 <img src="../src/img/cuvette.jpg" class="shopping-img" alt="fish-toilets" width="80%">
                 <h3>Cuvette qui donne la pêche</h3>
                 <p class="price">13.90 €</p>
-                <input class="submit-button" type="button" value="Acheter">
-            </article>
+                <a href="panier.php?action=ajout&amp;l=LIBELLEPRODUIT&amp;q=QUANTITEPRODUIT&amp;p=PRIXPRODUIT" onclick="window.open(this.href, '', 
+                'toolbar=no, location=no, directories=no, status=yes, scrollbars=yes, resizable=yes, copyhistory=no, width=600, height=350'); return false;">Ajouter au panier</a>
+            </form>
 
-            <article id="cat-fish">
+            <form id="cat-fish">
                 <img src="../src/img/poisson-pour-chat.jpg" class="shopping-img" width=80% alt="poisson-pour-chat">
                 <h3>Poisson en plastique pour chat</h3>
                 <p class="price">8.99 €</p>
-                <input class="submit-button"type="button" value="Acheter">
-            </article>
+                <a href="panier.php?action=ajout&amp;l=LIBELLEPRODUIT&amp;q=QUANTITEPRODUIT&amp;p=PRIXPRODUIT" onclick="window.open(this.href, '', 
+                'toolbar=no, location=no, directories=no, status=yes, scrollbars=yes, resizable=yes, copyhistory=no, width=600, height=350'); return false;">Ajouter au panier</a>
+            </form>
           
         </div>
-
+        <script type="text/javascript" src="script.js"></script>
     </section>
+ 
 </body>
 </html>
