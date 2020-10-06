c5232b1fd81941044f576cb018c32e70c20895e2
Ali <ma.ramzan089@gmail.com>
2020-10-06 15:24:03 +0200

    nav bar ok

--- src/script.js
+++ src/script.js
@@ -56,3 +56,27 @@ function createCard(title, imageUrl, linkUrl) {
 for (let i = 0; i < articles.length; i++) {
   createCard(articles[i].mytitle, articles[i].picture, articles[i].myLink);
 }
+
+
+window.onscroll = function () {
+
+  if (document.documentElement.scrollTop > 100) {
+      document.getElementById("navbar").style.background="#63868c";
+      document.getElementById("navbar-right").style.paddingTop = "20px";      
+      document.getElementById("logo").style.display = "flex";
+      document.getElementById("navbar-right").style.color = "#63868c";
+      document.getElementById("navbar-right").style.display = flex;
+      document.getElementById("navbar-right").style.alignItems = alignItems;
+      
+      
+      
+  }
+  else {
+      document.getElementById("navbar").style.background = "transparent";  
+      document.getElementById("logo").style.display = "flex";
+      document.getElementById("logo").style.alignItems = "center";
+      document.getElementById("navbar-right").style.alignItems = "center";
+  }
+
+
+}
