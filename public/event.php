3d1b38c6dd6842d269d1c3b49c978f7682d95053
maximilienpy <max314116@gmail.com>
2020-10-08 10:44:25 +0200

    fin JS, début CSS avec flex box, pour card aligné horizontalement

--- public/event.php
+++ public/event.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="../src/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet"> 
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet"> 
-    <title>Les compèts  -- events</title>
+    <title>Les compétitions</title>
 
 
 </head>
@@ -30,9 +30,25 @@
                 <div class="cardHeaderbis">
                     <div class="cardPic"></div>
                     <div class="cardBodybis">
-                        <h4 class="cardTitlebis">Montréal 2021</h4>
-                        <p class="cardTexte">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>
+                        <h4 class="cardTitlebis">Montréal Gaming Tour 2021</h4>
+                        <p class="cardTexte">Les saumons-enflammés sont à nouveau « On The Web Again » pour participer à la célèbre compétition d’E-Sport de Montréal 2021.
+                        Comme chaque année l’intégralité des compétitions, auxquelles participeront les Saumons-Enflammés seront retransmises en direct par le site MLG.com, partenaire de la compétition du Montréal Gaming Tour 2021.
+                        Clique sur le lien pour assister à cet évènement exceptionnel.<p>
                         <a class="cardLinkbis" href="https://www.mlg.com/schedule"><button class="cardButtonbis">Voir article</button></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="cardEvent">
+            <div class="cardArticle">
+                <div class="cardHeaderbis">
+                    <div class="cardPicbis"></div>
+                    <div class="cardBodybis">
+                        <h4 class="cardTitlebis">Le restaurant des Saumons-Enflammés : Le rendez-vous gastronomique</h4>
+                        <p class="cardTexte">Les saumons-enflammés ne cessent pas de nous faire découvrir de nouveaux lieux à la convivialité toujours plus grande, avec la « touch » insolite qui les caractérise si bien. Ils feront une démo de leurs talents de pêcheurs, au restaurant ZAOA à NEW YORK le 28-11-2020 à partir de 11h00.</p>
+                        <p class="cardTexte">Voir le lien pour plus d’information.</p>
+                        <a class="cardLinkbis" href="https://ny.eater.com/2018/10/9/17956126/zauo-photos-menu-japanese-restaurant-fish-nyc"><button class="cardButtonbis">Voir article</button></a>
                     </div>
                 </div>
             </div>
