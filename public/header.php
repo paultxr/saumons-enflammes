8ef60d3f53f20b5395ca89f150edb4e2b9c4ae3f
Ali <ma.ramzan089@gmail.com>
2020-10-11 10:48:53 +0200

    finish

--- public/header.php
+++ public/header.php
@@ -1,5 +1,13 @@
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../src/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Nav bar responsive</title>
+</head>
+<body>
     <header id="navbar">      
         <div class="logo" id="logo"><a href="home.php"><img src="../src/img/saumonslogo.png" ></a></div>
         <input id="toggler" class="toggler" type="checkbox" >
@@ -7,13 +15,15 @@
             <span class="ifa"><i class="fa fa-bars"></i></span>
         </label>    
         <nav id="navbar-right">    
-            <ul>
-                <li><a href="home.php">Accueil</a></li>
-                <li><a href="team.php">Notre Equipe</a></li>
-                <li><a href="stream.php">En Direct</a></li>
-                <li><a href="event.php">Évènements</a></li>
-                <li><a href="shop.php">Boutique</a></li>  
+            <ul id="navul">
+                <li><a id="navli" href="home.php">Accueil</a></li>
+                <li><a id="navli"href="team.php">Notre Equipe</a></li>
+                <li><a id="navli"href="stream.php">En Direct</a></li>
+                <li><a id="navli"href="event.php">Évènements</a></li>
+                <li><a id="navli"href="shop.php">Boutique</a></li>  
             </ul>
         </nav>
     </header>
-   
+    <script src="../src/script.js"></script>
+</body>
+</html>
